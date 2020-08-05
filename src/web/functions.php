f7218aeb21d4b12ba1a317148965b355ad16ec71
Nikolai Nechai <39387943+Nikonotea@users.noreply.github.com>
2020-08-05 15:35:13 +0300

    Add functions for web task

--- src/web/functions.php
+++ src/web/functions.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 /**
  * The $airports variable contains array of arrays of airports (see airports.php)
  * What can be put instead of placeholder so that function returns the unique first letter of each airport name
@@ -6,12 +9,27 @@
  *
  * Create a PhpUnit test (GetUniqueFirstLettersTest) which will check this behavior
  *
- * @param  array  $airports
+ * @param  array $airports
  * @return string[]
  */
-function getUniqueFirstLetters(array $airports)
+function getUniqueFirstLetters(array $airports): array
 {
-    // put your logic here
+    $allFirstLetters = [];
+    foreach ($airports as $airport) {
+        $allFirstLetters[] = ucfirst($airport['name'][0]);
+    }
+    $uniqueFirstLetters = array_unique($allFirstLetters);
+    sort($uniqueFirstLetters);
+    return $uniqueFirstLetters;
+}
 
-    return ['A', 'B', 'C'];
+function getAirportsByFirstLetter(array $airports, string $letter): array
+{
+    $airportsByFirstLetter = [];
+    foreach ($airports as $airport) {
+        if ($airport['name'][0] === $letter) {
+            $airportsByFirstLetter[] = $airport;
+        }
+    }
+    return $airportsByFirstLetter;
 }
